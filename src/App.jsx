ea8386912a2c3004ad0fd00952545d142e9c6f6c
KaterinaS. <153716458+KaterinaSelem@users.noreply.github.com>
2024-04-17 21:44:57 +0200

    homework02

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
-import Lesson02 from "./lessons/Lessons02/Lesson02";
+// import Lesson02 from "./lessons/Lessons02/Lesson02";
+import Homework02 from "./homeworks/homework02/homework02/Homework02";
 
 function App() {
   return (
     <div className="App">
-<Lesson02 />
+{/* <Lesson02 /> */}
+<Homework02 />
     </div>
   );
 }

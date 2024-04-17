2ef1734d126d66a159d706edbebd226563529712
KaterinaS. <153716458+KaterinaSelem@users.noreply.github.com>
2024-04-17 20:28:48 +0200

    lesson2

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,15 @@
+import "./style.css";
+
+function Button() {
+  const sendButtonName = "Send";
+  const getButtonName = "Get";
+  const isGetButton = true;
+
+  return (
+  <button className="main-button">
+    {isGetButton ? getButtonName : sendButtonName} data
+    </button>
+);
+}
+
+export default Button;
---
+++ src/lessons/Lessons02/Lesson02.jsx
@@ -0,0 +1,14 @@
+import "./style.css";
+import Button from "../../components/Button/Button";
+
+function Lesson02 () {
+    return(
+        <div className="buttons-container">
+        <Button />
+        <Button />
+        <Button />
+      </div>  
+    )
+}
+
+export default Lesson02; 

abffaa543248c8ab13f20ccdb09b132522ed7a48
KaterinaS. <153716458+KaterinaSelem@users.noreply.github.com>
2024-04-21 22:21:15 +0200

    lesson04

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,23 @@
+// import "./styles.css";
+
+// function Button({ buttonName = "get data", isPrimaryButton }) {
+//   console.log();
+
+//   const buttonClass = `main-button ${isPrimaryButton ? "primary-button" : "secondary-button"}`;
+
+//   return <button className={buttonClass}>{buttonName}</button>;
+// }
+
+// export default Button;
+
 import "./styles.css";
 
-function Button({ buttonName = "get data", isPrimaryButton }) {
-  console.log();
-
-  const buttonClass = `main-button ${isPrimaryButton ? "primary-button" : "secondary-button"}`;
-
-  return <button className={buttonClass}>{buttonName}</button>;
+function Button({ name, type = "button", onButtonClick }) {
+  return (
+    <button className="button-component" type={type} onClick={onButtonClick}>
+      {name}
+    </button>
+  );
 }
 
 export default Button;
---
+++ src/homeworks/Homework03/Homework03.jsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+import LoginForm from "../../components/loginform/LoginForm";
+
+function Homework03() {
+    return (
+    <div>
+        <LoginForm/>
+    </div>
+    );
+}
+
+export default Homework03;
